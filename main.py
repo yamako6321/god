1835983b913f9d9d3ba353e2ff288d6c5a83b174
yamako6321 <124650861+yamako6321@users.noreply.github.com>
2023-03-12 14:03:25 +0700

    wkedefjgbifoerwp

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello world')
+n_days = int(input())
+n_second = n_days * 60 * 60
+v_hour = 38241 #miles per hour
+v_second = v_hour/3600 #miles per second
+length = v_second * n_second #in miles
+print(f'in miles: {length}, in kilometers: {length * 1.609344}, in astronomical units: {length * 1.609344 * 149597870.7}')
+
